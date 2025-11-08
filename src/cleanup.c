b833011943c701c739839baab20e00dff1d3efb0
Mohamed <mohamedhadodo33@gmail.com>
2025-11-08 14:41:10 +0000

    Add shell initialization and main loop
    - init_shell(): Initialize shell state and environment
    - cleanup_shell(): Free all resources on exit
    - main.c: REPL loop with readline integration
    - Proper signal setup and handling
    - Global variable g_signal_received definition
    - Exit on EOF (Ctrl+D) or exit command

---
+++ src/cleanup.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cleanup.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhaddadi <mhaddadi@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/08 14:31:09 by mhaddadi          #+#    #+#             */
+/*   Updated: 2025/11/08 14:31:30 by mhaddadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "minishell.h"
+
+void	cleanup_shell(t_shell *shell)
+{
+	if (!shell)
+		return ;
+	if (shell->env_list)
+		free_env(shell->env_list);
+	rl_clear_history();
+	free(shell);
+}
---
+++ src/main.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhaddadi <mhaddadi@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/08 14:31:25 by mhaddadi          #+#    #+#             */
+/*   Updated: 2025/11/08 14:31:30 by mhaddadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "minishell.h"
+
+volatile sig_atomic_t	g_signal_received = 0;
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_shell	*shell;
+	char	*line;
+
+	(void)argc;
+	(void)argv;
+	shell = init_shell(envp);
+	if (!shell)
+	{
+		print_error("minishell", NULL, "initialization failed");
+		return (ERROR);
+	}
+	while (shell->running)
+	{
+		line = readline("minishell$ ");
+		if (!line)
+		{
+			printf("exit\n");
+			break ;
+		}
+		if (*line)
+			add_history(line);
+		free(line);
+	}
+	cleanup_shell(shell);
+	return (SUCCESS);
+}
