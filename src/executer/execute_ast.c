4994259ba70c94dea03a65b6bd16e1d583a4294d
KikoSilva1 <francisco@Administrator.localdomain>
2025-12-30 18:46:50 +0000

    add a function to calculate the number of pipes in a pipeline

--- src/executer/execute_ast.c
+++ src/executer/execute_ast.c
@@ -16,19 +16,25 @@
 /* Forward declarations */
 static int	execute_pipeline(t_ast *node, t_shell *shell);
 
-
-void configure_stds(int pipe_indice)
+//pipe_indice = pipe a se escrever 
+void configure_stds(int *pipes, int pipe_indice, int numb_of_pipes)
 {
-	if (pipe_indice > 0) 
-		dup2(pipes[(i - 1) * 2], STDIN_FILENO);
+	int read_fd;
+	int write_fd;
+	//fd[0] (Leitura)
+	//fd[1] (Escrita)
+	read_fd = (pipe_indice - 1) * 2; //é o read_fd do pipe anterior
+	write_fd = (pipe_indice * 2) - 1; // e o write fd do pipe a se escrever(pipe_indice)
+	if (pipe_indice > 0) //pipe indice descreve o pipe a se escrever, se por o primeiro pipe, e o primeiro cmd entao nao leio de nenhum pipe
+		dup2(pipes[read_fd], STDIN_FILENO);
 
-	if (pipe_indice < num_cmds - 1) //escreve para o pipe a frente
-		dup2(pipes[i * 2 + 1], STDOUT_FILENO);
+	if (pipe_indice < numb_of_pipes - 1 ) //apenas ate ao penultimo pipe
+		dup2(pipes[write_fd], STDOUT_FILENO);
 }
 
 
 
-void handle_cmd(t_ast *curr_node, int *pipe, int pipe_indice)
+void handle_cmd(t_ast *curr_node, int *pipes, int pipe_indice, int numb_of_pipes)
 {
 	int		heredoc_pipe_read_fd;
 	int		pid;
@@ -40,7 +46,7 @@ void handle_cmd(t_ast *curr_node, int *pipe, int pipe_indice)
 
 	if (pid == 0) // child
 	{
-		configure_stds(pipe_indice, numb _of_pipes);//nmb_of_pipes is important to check the case of the last and first cmd of pipeline
+		configure_stds(pipes, pipe_indice, numb_of_pipes);//nmb_of_pipes is important to check the case of the last and first cmd of pipeline
 		// fechar todos os pipes no child          //(first cmd does not read from a pipe and last does not write to pipe)
 		close_all_pipes(pipes, (num_cmds - 1));
 		// executar comando
@@ -49,7 +55,7 @@ void handle_cmd(t_ast *curr_node, int *pipe, int pipe_indice)
 	}
 }
 
-void handle_subshell()
+void handle_subshell(t_ast *curr_node, int *pipes, int pipe_indice, int numb_of_pipes)
 {
 	int		pid;
 	pid = fork();
@@ -58,7 +64,7 @@ void handle_subshell()
 
 	if (pid == 0) // child
 	{
-		configure_stds(pipe_indice, numb _of_pipes);//nmb_of_pipes is important to check the case of the last and first cmd of pipeline
+		configure_stds(pipes, pipe_indice, numb_of_pipes);//nmb_of_pipes is important to check the case of the last and first cmd of pipeline
 		// fechar todos os pipes no child          //(first cmd does not read from a pipe and last does not write to pipe)
 		close_all_pipes(pipes, (num_cmds - 1));
 		// executar comando
@@ -67,26 +73,44 @@ void handle_subshell()
 	}
 }	
 
-
-void traverse_and_execute(t_ast *node, int *pipes, int *i, int num_pipes)//this runs always in a pipeline
+//se encontro um && ou || numa pipeline e garantidamente um subshell, nao conto os pipes desse subshell
+//porque se fosse um  && ou || divisor de pipelines teria sifdo apanhado em execute ast:
+// a | b && c | d  vs  a |( b && c )| d
+void traverse_and_execute(t_ast *node, int *pipes, int *pipe_indice, int num_pipes)//this runs always in a pipeline
 {
-    if (!node) return;
+    if (!node)
+		return;
 
     if (node->type == PIPE) {
         // Primeiro vai à esquerda (pode haver outro PIPE ou um CMD)
-        traverse_and_execute(node->left, pipes, i, num_pipes);
+        traverse_and_execute(node->left, pipes, pipe_indice, num_pipes);
         // Depois vai à direita
-        traverse_and_execute(node->right, pipes, i, num_pipes);
+        traverse_and_execute(node->right, pipes, pipe_indice, num_pipes);
     } 
     else {
 		if (node -> type == CMD)
-        	handle_cmd(node, pipes, *i, num_pipes);
+        	handle_cmd(node, pipes, *pipe_indice, num_pipes);
 		else if(node -> type == AND || node -> type == OR)
-			handle_subshell(node, pipes, *i, num_pipes); //esta subshell pode ter pipelines proprias dentro
-        (*i)++; // Avança para o próximo comando no pipeline
+			handle_subshell(node, pipes, *pipe_indice, num_pipes); //esta subshell pode ter pipelines proprias dentro
     }
 }	
 
+
+int calc_numb_pipes(t_ast *node)
+{
+	int count;
+	count = 0;
+	
+	if (node ->type == CMD)
+		return (count);
+	if (node -> type == AND || node -> type == OR)
+		return (count);
+	if(node ->type == PIPE)
+		count++;
+	count = count + calc_numb_pipes(node -> left);
+	count = count + calc_numb_pipes(node -> right);
+	return (count);
+}
 //1- contar todos os pipes da pipeline : "a | (b | c || d | e) | f"  tem apenas 2 pipes. 
 //2 - percorrer a pipeline, se for um comando dou setup para ler do pipe atras e escrever no da frente.
 //    se for uma expressao(and ou or chamo a funcao execute_ast() depois de setar os std fds),
@@ -98,11 +122,13 @@ static int execute_pipeline(t_ast *node, t_shell *shell)
 	int		status;
 	int		last_status;
 	t_ast	*curr_node;
+	int		pipe_indice;
 
-	numb_of_pipes = calc_nmb_pipes(node); //node em principio e um pipe, calcula o nmb of pipes desta pipeline.
+	pipe_indice = 0;
+	numb_of_pipes = calc_nmb_pipes(node); //calcula o nmb of pipes desta pipeline.
 	pipes = create_pipes(numb_of_pipes);
 	//ao percorrer a tree envio o pipe e o indice do pipe;
-	transverse_tree_and_execute();
+	transverse_tree_and_execute(node,pipes,pipe_indice,numb_of_pipes);
 	//fechar pipes
 	//retornar status 
 }
